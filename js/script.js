74c446216deea4b481d3394938648aa82ad0ce83
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-14 16:51:43 +0100

    add: last bonus

--- js/script.js
+++ js/script.js
@@ -10,33 +10,54 @@ console.log(sliderItemsContainer, nextUp, prevDown); //debug
 for (let i = 0; i < userImages.length; i++) {
     
     let currentImg = userImages[i];
-
+    
     sliderItemsContainer.innerHTML +=  
     `<div class="item">
+    <img src="${currentImg}" alt="">
+    </div>`;
+};
+
+
+//ULTIMO SUPER BONUS
+let thumbNailsItems = document.querySelector(".thumbnails-image");
+for (let i = 0; i < userImages.length; i++) {
+    
+    let currentImg = userImages[i];
+
+    thumbNailsItems.innerHTML +=  
+    `<div class="thumb-item">
         <img src="${currentImg}" alt="">
     </div>`;
 };
 
+
+
+//setto stato iniziale ultimo bonus
+const thumbNailsItemsArrey = document.querySelectorAll(".thumb-item")
+console.log(thumbNailsItemsArrey);
 //setto lo stato iniziale dove appare almeno la prima immagine
 const sliderItemsArray = document.querySelectorAll(".item");
-console.log(sliderItemsArray);
 let itemCounter = 0;
 sliderItemsArray[itemCounter].classList.add("active");
-
+thumbNailsItemsArrey[itemCounter].classList.add("active");
 //impostiamo il bottone next
 nextUp.addEventListener("click", function(){
     if (itemCounter === sliderItemsArray.length - 1){
         sliderItemsArray[itemCounter].classList.remove("active");
+        thumbNailsItemsArrey[itemCounter].classList.remove("active");
         itemCounter = 0; 
         sliderItemsArray[itemCounter].classList.add("active");
+        thumbNailsItemsArrey[itemCounter].classList.add("active");
     }else {
 
         //rimuovo classe precedente
         sliderItemsArray[itemCounter].classList.remove("active");
+        thumbNailsItemsArrey[itemCounter].classList.remove("active");
         //aumento il contantore
         itemCounter++;
         //aggiungo la classe  al nuovo item
         sliderItemsArray[itemCounter].classList.add("active");
+        thumbNailsItemsArrey[itemCounter].classList.add("active");
     }
 });
 
@@ -45,15 +66,19 @@ prevDown.addEventListener("click", function(){
     //rimuovo classe precedente
     if (itemCounter === 0){
         sliderItemsArray[itemCounter].classList.remove("active");
+        thumbNailsItemsArrey[itemCounter].classList.remove("active");
         itemCounter = sliderItemsArray.length - 1;
         sliderItemsArray[itemCounter].classList.add("active");
+        thumbNailsItemsArrey[itemCounter].classList.add("active");
 
     }else {
 
         sliderItemsArray[itemCounter].classList.remove("active");
+        thumbNailsItemsArrey[itemCounter].classList.remove("active");
         //aumento il contantore
         itemCounter--;
         //aggiungo la classe  al nuovo item
         sliderItemsArray[itemCounter].classList.add("active");
+        thumbNailsItemsArrey[itemCounter].classList.add("active");
     };
 });
